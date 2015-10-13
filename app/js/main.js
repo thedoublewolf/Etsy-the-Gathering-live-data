393155a517d8160a1e70aca03a7a36be9b45be06
Jonny Warren <jonnywarren06@gmail.com>
2015-10-12 21:19:18 -0400

    Debugging of hover icons completed!

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,9 @@
   });
 
   // RENDER ITEMS ON PAGE
-  var template = $('#itemListing').html();
-  var render = _.template(template);
+
+  var templateString = $('#itemListing').html();
+  var renderTemplate = _.template(templateString);
 
   var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=Magic+the+Gathering&includes=Images,Shop';
 
@@ -21,21 +22,19 @@
     dataType: 'jsonp',
     method: 'get'
   }).then(function (response) {
-    var templateString = $('#itemListing').text();
-    var renderTemplate = _.template(templateString);
     _.each(response.results, function (item) {
       var itemHTML = renderTemplate(item);
       $('.magicStuff').append(itemHTML);
     });
   });
 
-  $('#magicItem').mouseover('.heartHam', function (event) {
+  $('.magicStuff').on('mouseenter', '#magicItem', function (event) {
     console.log(event);
     $(this).find('#heart').removeClass('heartHidden').addClass('heartShown');
     $(this).find('#hamburger').removeClass('hamburgerHidden').addClass('hamburgerShown');
   });
 
-  $('#magicItem').mouseout('.heartHam', function (event) {
+  $('.magicStuff').on('mouseleave', '#magicItem', function (event) {
     console.log(event);
     $(this).find('#heart').addClass('heartHidden').removeClass('heartShown');
     $(this).find('#hamburger').addClass('hamburgerHidden').removeClass('hamburgerShown');
--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@
 
 
 // RENDER ITEMS ON PAGE
-  var template = $('#itemListing').html();
-  var render = _.template(template);
+
+  var templateString = $('#itemListing').html();
+  var renderTemplate = _.template(templateString);
 
   var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=Magic+the+Gathering&includes=Images,Shop';
 
@@ -20,22 +21,20 @@
   	dataType: 'jsonp',
   	method: 'get'
   	}).then (function (response){
-  	  var templateString = $('#itemListing').text();
-  		var renderTemplate = _.template(templateString);
   		_.each(response.results, function (item){
   	 		var itemHTML = renderTemplate(item);
   			$('.magicStuff').append(itemHTML);
-		});
+		})
   });
 
 
-  $('#magicItem').mouseover( '.heartHam', function (event){
+  $('.magicStuff').on('mouseenter', '#magicItem', function (event){
   	console.log(event);
   	$(this).find('#heart').removeClass('heartHidden').addClass('heartShown');
   	$(this).find('#hamburger').removeClass('hamburgerHidden').addClass('hamburgerShown');
   });
 
-  $('#magicItem').mouseout( '.heartHam', function (event){
+  $('.magicStuff').on('mouseleave', '#magicItem', function (event){
   	console.log(event);
   	$(this).find('#heart').addClass('heartHidden').removeClass('heartShown');
   	$(this).find('#hamburger').addClass('hamburgerHidden').removeClass('hamburgerShown');
